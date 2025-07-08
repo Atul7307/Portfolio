c9c1f03d595ea2d36dd005a0e9eb5db16c66c454
Atul Kesharwani <atulkesharwani7974@gmail.com>
2025-07-08 17:12:04 +0530

    adding some changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import clsx from "clsx";
 import FireFliesBackground from "@/components/FireFliesBackground";
 import Sound from "@/components/Sound";
-import Footer from "@/components/Footer"; 
+import Footer from "@/components/Footer";
 import HomeBtn from "@/components/HomeBtn";
 
 const inter = Inter({
@@ -19,8 +19,8 @@ export const metadata = {
       "Atul`s Portfolio",
   },
   icons: {
-    icon:"/favicon.ico",
-    shortcut: "/favicon.ico", 
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
   },
   description:
     "A unique creative portfolio designed by Atul with cutting-edge technologies like Next.js, Tailwind CSS, Three.js, and Framer Motion. Experience the art of modern web development firsthand.",
@@ -29,6 +29,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2932539371789323"
+          crossorigin="anonymous"></script>
+      </head>
       <body
         className={clsx(
           inter.variable,
@@ -39,7 +43,7 @@ export default function RootLayout({ children }) {
         <HomeBtn />
         <Sound />
         {children}
-        <Footer /> 
+        <Footer />
         <div id="my-modal" />
       </body>
     </html>
